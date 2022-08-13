9dc808949a962ef757fedf25beb6f5e552402ded
AndrPerep <79010353+AndrPerep@users.noreply.github.com>
2022-08-13 15:22:22 +0300

    reduce db queries number.

--- blog/views.py
+++ blog/views.py
@@ -30,7 +30,7 @@ def serialize_post_optimized(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'tags': [serialize_tag(tag) for tag in post.tags.all().annotate(posts_count=Count('posts'))],
         'first_tag_title': post.tags.all()[0].title,
     }
 
@@ -38,14 +38,14 @@ def serialize_post_optimized(post):
 def serialize_tag(tag):
     return {
         'title': tag.title,
-        'posts_with_tag': len(Post.objects.filter(tags=tag)),
+        'posts_with_tag': tag.posts_count,  #len(Post.objects.filter(tags=tag)),
     }
 
 
 def index(request):
     most_popular_posts = Post.objects.popular().prefetch_related('author')[:5].fetch_with_comments_count()
 
-    most_fresh_posts = Post.objects.prefetch_related('author').order_by('-published_at')[:5]
+    most_fresh_posts = Post.objects.prefetch_related('author', 'tags').order_by('-published_at')[:5]
     most_fresh_posts_ids = [post.id for post in most_fresh_posts]
 
     posts_with_comments = Post.objects.filter(id__in=most_fresh_posts_ids).annotate(comments_count=Count('comments'))
