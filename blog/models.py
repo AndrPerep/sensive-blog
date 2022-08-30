e85fe0e4be1547fc468088215b4fede6920cc978
AndrPerep <79010353+AndrPerep@users.noreply.github.com>
2022-08-30 19:21:56 +0300

    rename `posts_ids`

--- blog/models.py
+++ blog/models.py
@@ -9,9 +9,9 @@ class PostQuerySet(models.QuerySet):
         return Post.objects.annotate(likes_count=models.Count('likes')).order_by('-likes_count')
 
     def fetch_with_comments_count(self):
-        most_popular_posts_ids = [post.id for post in self]
+        posts_ids = [post.id for post in self]
 
-        posts_with_comments = Post.objects.filter(id__in=most_popular_posts_ids).annotate(
+        posts_with_comments = Post.objects.filter(id__in=posts_ids).annotate(
             comments_count=models.Count('comments'))
         ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
         count_for_id = dict(ids_and_comments)
